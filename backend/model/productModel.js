4ee42ffdbe5949cf335a378bdf3065fb29f4c33c
Hellv99 <oluwafeyisolaayoola@gmail.com>
2025-07-12 18:16:55 +0100

    updating files
    made some changes to productmodel, server and db.js

---
+++ backend/model/productModel.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Product name is required"],
+    },
+    description: {
+      type: String,
+      required: [true, "Product description is required"],
+    },
+    price: {
+      type: Number,
+      required: [true, "Product price is required"],
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+
+    category: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model("Product", productSchema);
+export default Product;
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,32 @@
 import express from "express";
-import mongoose from "mongoose";
 import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
 
 dotenv.config();
 const app = express();
 
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error(err));
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API Running");
+app.post("/api/products", async (req, res) => {
+  const product = req.body;
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "please provide all fields" });
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.error("Error in creating product", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+app.listen(5000, () => {
+  connectDB();
+  console.log(`App listening on at http://localhost:5000`);
+});
