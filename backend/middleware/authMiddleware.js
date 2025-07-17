4fa438e5b85a10f23452671731823009de2f1f8c
Hellv99 <oluwafeyisolaayoola@gmail.com>
2025-07-17 04:08:47 +0100

    update authmiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -10,8 +10,14 @@ export default function authMiddleware(req, res, next) {
 
   try {
     // Verify token
+    console.log("Token received in middleware:", token); // Log the token
+    console.log(
+      "Secret used in middleware:",
+      process.env.JWT_SECRET ? "Loaded" : "Undefined"
+    ); // Check secret here too
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // Attach user info to request object
+    req.user = decoded.user; // Attach user info to request object
     next(); // Proceed to the next middleware or route handler
   } catch (err) {
     res.status(401).json({ msg: "Token is not valid" });
