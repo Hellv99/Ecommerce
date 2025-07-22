098087d8212860fd886396dcdff98a891aa40bc9
Hellv99 <oluwafeyisolaayoola@gmail.com>
2025-07-22 15:54:35 +0100

    added ordermodel,orderroute

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import { connectDB } from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 dotenv.config();
 const app = express();
 
@@ -14,6 +15,8 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
 app.use("/api/products", productRoutes);
+
+app.use("/api/orders", orderRoutes);
 
 app.listen(5000, () => {
   connectDB();
